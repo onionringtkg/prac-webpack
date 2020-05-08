cc7ff710a315c547ae458e555df680ee5c71e97f
Okano Takumi <oknokn.of.12@gmail.com>
2020-05-08 09:20:23 +0900

    learn moudules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import _ from 'lodash'
+import * as utilities from './utilities'
+
+console.log(utilities.Nizyou(10))
+console.log(utilities.Nizyou(5))
 
 function component() {
     const element = document.createElement('div');
-    const array = ["Hello", "webpack", "welcome!!"];
+    const array = ["Hello", "webpack", "welcome!!", utilities.NAME];
     element.innerHTML = _.join(array, ', ');
     return element;
 }
