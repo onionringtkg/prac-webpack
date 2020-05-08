6d9d98f86f07f5b5fb483611ca35fe4ea6f52615
Okano Takumi <oknokn.of.12@gmail.com>
2020-05-08 11:05:55 +0900

    learn some laoders

---
+++ src/utilities.js
@@ -0,0 +1,11 @@
+export function Nizyou(num) {
+    return num ** 2;
+}
+
+export const NAME = "Taro"
+
+export default class Lion {
+    static say() {
+        console.log("Hi! I'm Lion");
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+const path = require('path');
 
-const outputPath = path.resolve(__dirname, 'dist')
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
     //webpackで使用するfileと、出力先の指定
@@ -9,8 +9,38 @@ module.exports = {
         filename: 'main.js',
         path: outputPath
     },
+    //laoderの登録（testにuseで指定する適応させるファイルの記載）
+    module: {
+        rules: [
+            { 
+                test: /\.css$/, 
+                use: [
+                    //reverse order で実行されるので記述順に注意
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            { 
+                test: /\.scss$/, 
+                use: [
+                    //reverse order で実行されるので記述順に注意
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|ico|svg)$/i, 
+                loader: 'url-loader',
+                options: {
+                    limit: 2048,
+                    name: './images/[name].[ext]'
+                }
+            }
+        ]
+    },
     //起動時にページを開くように指定
     devServer: {
         contentBase: outputPath
     }
-}
+};
